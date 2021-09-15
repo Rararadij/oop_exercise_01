b65defd82eea663190b025bd31e7b194ead89571
Rararadij <84455568+Rararadij@users.noreply.github.com>
2021-09-15 13:10:02 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Long {
+public:
+    unsigned int first_number;
+    unsigned int second_number;
+};
+
+
+
+
+
+int main()
+{
+    Long Chislo;
+
+    cout << "Enter two numbers:\n";
+    cin >> Chislo.first_number >> Chislo.second_number;
+
+    string operation;
+    cout << "Choose operation:\n+\n-\n*\n/\n%\n>\n";
+    cin >> operation;
+
+    cout << "Answer is: ";
+    if (operation == "+")
+        cout << Chislo.first_number + Chislo.second_number;
+    if (operation == "-")
+        cout << Chislo.first_number - Chislo.second_number;
+    if (operation == "*")
+        cout << Chislo.first_number * Chislo.second_number;
+    if (operation == "/")
+        cout << Chislo.first_number / Chislo.second_number;
+    if (operation == "%")
+        cout << Chislo.first_number % Chislo.second_number;
+    if (operation == ">") 
+        if (Chislo.first_number == Chislo.second_number)
+            cout << Chislo.first_number << " = " << Chislo.second_number;
+        if (Chislo.first_number > Chislo.second_number)
+            cout << Chislo.first_number << " > " << Chislo.second_number;
+        if (Chislo.first_number < Chislo.second_number)
+            cout << Chislo.first_number << " < " << Chislo.second_number;   
+}
